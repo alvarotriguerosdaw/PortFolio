34c3fccc60b022aa957ccc14d9f2ee7aaf8a651a
alvarotriguerosdaw <alvarotriguerosdaw@gmail.com>
2025-07-04 18:33:14 +0200

    Añadido script escritura

---
+++ public/script/typed.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  new Typed('#element', {
+    strings: ['Desarrollador de Aplicaciones Web'],
+    typeSpeed: 75,           
+    startDelay: 300,       
+    backSpeed: 75,          
+    smartBackspace: true,    
+    shuffle: false,          
+    backDelay: 1500,         
+    loop: true,              
+    loopCount: false,        
+    showCursor: true,        
+    cursorChar: '|',         
+    contentType: 'html'      
+  });
+});
