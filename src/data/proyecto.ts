fd977b3dea9a9542d7d3af7081574a50717c3441
alvarotriguerosdaw <alvarotriguerosdaw@gmail.com>
2025-07-10 12:54:53 +0200

    props proyectos

---
+++ src/data/proyecto.ts
@@ -0,0 +1,36 @@
+
+export const proyectoData = [
+
+    {
+        title: "Proyecto marea",
+        descripcion:"estes es mi nuevo proyecto de desarrollo",
+        imagen: "/Perfil.png",
+        tecnologias:[
+            "Astro", "React", "Tailwind"
+        ],
+        github:"https://github.com/alvarotriguerosdaw",
+        enlace:"https://colorhunt.co/"
+    },
+    {
+        title: "Proyecto nena",
+        descripcion:"estes es mi nuevo proyecto de desarrollo",
+        imagen: "/Perfil.png",
+        tecnologias:[
+            "Astro", "JavaScript", "Java"
+        ],
+        github:"https://github.com/alvarotriguerosdaw",
+        enlace:"https://github.com/alvarotriguerosdaw"
+    },
+    {
+        title: "Proyecto nena",
+        descripcion:"estes es mi nuevo proyecto de desarrollo",
+        imagen: "/Perfil.png",
+        tecnologias:[
+            "Astro", "JavaScript", "Java"
+        ],
+        github:"https://github.com/alvarotriguerosdaw",
+        enlace:"https://github.com/alvarotriguerosdaw"
+    },
+    
+
+]
